ddfcb78c63c4581a7d0e9499f15c47758ec71ce2
shengyu <usungu@gmail.com>
2022-08-21 00:15:42 +0800

    Set random color

--- components/ThreeDemo/Box.tsx
+++ components/ThreeDemo/Box.tsx
@@ -5,46 +5,41 @@ import * as THREE from "three";
 export function Box(props: ThreeElements["mesh"]) {
   const ref = useRef<THREE.Mesh>(null!);
   const [clicked, click] = useState(false);
-  const [direction, setDirection] = useState(1);
-  const [yDirection, setYDirection] = useState(1);
-  const [zDirection, setZDirection] = useState(1);
+  let direction = 1;
+  let yDirection = 1;
+  let zDirection = 1;
 
   useFrame((state, delta) => {
     ref.current.rotation.x += 0.01;
-    if (ref.current.position.x > 20) {
-      setDirection(-1);
+    if (ref.current.position.x > 10) {
+      direction = -1;
     }
-    if (ref.current.position.x < -20) {
-      setDirection(1);
+    if (ref.current.position.x < -10) {
+      direction = 1;
     }
-    if (ref.current.position.y > 20) {
-      setYDirection(-1);
+    if (ref.current.position.y > 10) {
+      yDirection = -1;
     }
-    if (ref.current.position.y < -20) {
-      setYDirection(1);
+    if (ref.current.position.y < -10) {
+      yDirection = 1;
     }
-    if (ref.current.position.z > 20) {
-      setZDirection(-1);
+    if (ref.current.position.z > 10) {
+      zDirection = -1;
     }
-    if (ref.current.position.z < -20) {
-      setZDirection(1);
+    if (ref.current.position.z < -10) {
+      zDirection = 1;
     }
     ref.current.position.x += (Math.random() / 5) * direction;
     ref.current.position.y += (Math.random() / 6) * yDirection;
     ref.current.position.z += (Math.random() / 7) * zDirection;
   });
 
+  const color = new THREE.Color(Math.random(), Math.random(), Math.random());
+
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => {
-        setDirection(-1 * direction);
-      }}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={"hotpink"} />
+    <mesh {...props} ref={ref} scale={clicked ? 1.5 : 1}>
+      <boxGeometry args={[0.5, 0.5, 0.5]} />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 }
--- components/ThreeDemo/ThreeDemo.tsx
+++ components/ThreeDemo/ThreeDemo.tsx
@@ -5,18 +5,18 @@ import { Box } from "./Box";
 
 export function ThreeDemo() {
   const boxes = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 100; i++) {
     const x = THREE.MathUtils.randFloat(-20, 20);
     const y = THREE.MathUtils.randFloat(-20, 20);
     const z = THREE.MathUtils.randFloat(-20, 20);
-    boxes.push(<Box position={[x, y, z]} />);
+    boxes.push(<Box key={i} position={[x, y, z]} />);
   }
   return (
     <Canvas style={{ background: "#dcdcdc9e" }}>
       <ambientLight intensity={0.5} />
       <pointLight position={[50, 50, 50]} intensity={1} />
       <OrbitControls makeDefault enableDamping />
-      <primitive object={new THREE.AxesHelper(10)} />
+      <primitive object={new THREE.AxesHelper(20)} />
       {boxes}
     </Canvas>
   );
